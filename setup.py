239fb5d9f254da9feeb0a1221130cb2eb2d0a4f0
Tunnelrind <63593426+tunnelrind@users.noreply.github.com>
2025-03-12 12:20:30 +0100

    update to 1.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ksbanim',
-    version='1.0.22',
+    version='1.1.1',
     py_modules=['ksbanim'],
     install_requires=[
         'PyQt5',
@@ -12,12 +12,12 @@ setup(
         'triangle',
         'shapely'
     ],
-    author='Your Name',
-    author_email='your.email@example.com',
-    description='A module for animation using PyQt5, OpenGL, and imageio[ffmpeg]',
+    author='Biasini Dario',
+    author_email='tunnelrind@lernbaum.ch',
+    description='A module for animation based on PyQt5, OpenGL, and imageio[ffmpeg]',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/ksbanim',  # Update with your repository URL
+    url='https://github.com/tunnelrind/ksbanim', 
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
