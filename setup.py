bc2539ff6440aa6216dbb9284d0ab919cc67f9df
Tunnelrind <63593426+tunnelrind@users.noreply.github.com>
2025-06-24 12:49:38 +0200

    update to 1.1.38

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 setup(
     name='ksbanim',
-    version='1.1.37',
+    version='1.1.38',
     py_modules=['ksbanim'],
     install_requires=[
         'PyQt5',
