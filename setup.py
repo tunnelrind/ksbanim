2c3dcbc3c0bdcc611762109c4d2db89d24814519
Tunnelrind <63593426+tunnelrind@users.noreply.github.com>
2025-06-24 11:22:45 +0200

    update to 1.1.34

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup
-from pathlib import Path
+
+import os
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 setup(
     name='ksbanim',
@@ -17,7 +19,7 @@ setup(
     author='Biasini Dario',
     author_email='tunnelrind@lernbaum.ch',
     description='A module for animation based on PyQt5, OpenGL, and imageio[ffmpeg]',
-    long_description = Path(__file__).with_name("README.md").read_text(),
+    long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/tunnelrind/ksbanim', 
     classifiers=[
