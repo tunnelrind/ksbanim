9493ebdb9be17602bb3e979de9a5fc0063453622
Tunnelrind <63593426+tunnelrind@users.noreply.github.com>
2025-06-24 11:23:21 +0200

    update to 1.1.35

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 setup(
     name='ksbanim',
-    version='1.1.34',
+    version='1.1.35',
     py_modules=['ksbanim'],
     install_requires=[
         'PyQt5',
@@ -19,7 +19,7 @@ setup(
     author='Biasini Dario',
     author_email='tunnelrind@lernbaum.ch',
     description='A module for animation based on PyQt5, OpenGL, and imageio[ffmpeg]',
-    long_description=open('README.md').read(),
+    long_description=open('readme.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/tunnelrind/ksbanim', 
     classifiers=[
