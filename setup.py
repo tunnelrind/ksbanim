7a03edb11e4059da711bb4884ff546b7d5506db8
Tunnelrind <63593426+tunnelrind@users.noreply.github.com>
2025-07-18 11:07:28 +0200

    update to 1.1.45

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 setup(
     name='ksbanim',
-    version='1.1.44',
+    version='1.1.45',
     py_modules=['ksbanim'],
     install_requires=[
         'PyQt5',
