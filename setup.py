1a4176a4a640ab430e67116962387de67c136e92
Tunnelrind <63593426+tunnelrind@users.noreply.github.com>
2025-10-14 13:53:37 +0200

    update to 1.2.12

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 setup(
     name='ksbanim',
-    version='1.2.11',
+    version='1.2.12',
     py_modules=['ksbanim'],
     install_requires=[
         'PyQt5',
