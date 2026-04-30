6282858bc5f94012ffddb42e33211b6cbcfd861b
Tunnelrind <63593426+tunnelrind@users.noreply.github.com>
2026-04-30 09:03:25 +0200

    update to 1.2.23

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 setup(
     name='ksbanim',
-    version='1.2.22',
+    version='1.2.23',
     py_modules=['ksbanim'],
     install_requires=[
         'PyQt5',
